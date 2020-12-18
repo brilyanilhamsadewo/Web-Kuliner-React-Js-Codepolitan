c5d02396a204c30d270976b2b730adabb27a4851
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2020-12-18 23:19:15 +0700

    Memisahkan Navbar, Image, dan Welcome Message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import mainImage from "./assets/images/aneka-makanan.png";
 import CityCard from "./components/CityCard";
+import Navbar from './components/Navbar';
+import ImageAndWelcome from './components/ImageAndWelcome';
 
 function App() {
 
@@ -11,26 +13,9 @@ function App() {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
-        <a className="navbar-brand text-white" href="/">FoodParadise</a>
-      </nav>
+      <Navbar />
 
-      <div className="row" style={{ marginBottom: 30}}>
-        <img src={mainImage} width="100%" alt="Aneka Makanan" />
-      </div>
-
-      <div className="row">
-        <div className="col">
-          <div className="card text-white bg-success mb-3 text-center">
-            <div className="card-header"><h1>Welcome to FoodParadise</h1></div>
-            <div className="card-body">
-              <h4 className="card-title">The Easiest Way to Find Restaurants and Food</h4>
-              <p className="card-text">You can grab information about restaurants, cafees, cuisines with just a few clicks.</p>
-              <p className="card-text">Start by choosing the featured cities below, or search the city name.</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ImageAndWelcome />  
 
       <div className="container" style={{ marginTop: 30, marginBottom: 30 }}>
           <div className="row">
