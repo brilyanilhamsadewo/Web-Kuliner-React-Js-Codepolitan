e0b801e347275a6ee26d612d7b144e9caa30f59c
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2020-12-18 23:53:44 +0700

    Memisahkan Search City Menjadi Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import ImageAndWelcome from './components/ImageAndWelcome';
 import FeaturedCities from './components/FeaturedCities';
 import { Component } from 'react';
+import SearchCity from './components/SearchCity';
 
 class App extends Component{
   constructor() {
@@ -29,28 +30,10 @@ class App extends Component{
           <FeaturedCities cities={citiesDummy} />
   
           {/* start of search section */}
-          <div className="row" style={{ marginBottom: 30 }}>
-            <div className="col">
-              <h3>Search City</h3>
-              <div className="card">
-                <div className="card-body">
-                  <div className="form-row">
-                    <div className="col-11">
-                      <input 
-                      className="form-control" 
-                      type="text" 
-                      placeholder="Type keyword or city name" 
-                      value={this.state.keyword} 
-                      onChange={this.changeKeywordHandler}></input>
-                    </div>
-                    <div className="col-1">
-                      <button className="btn btn-primary" type="button">Search</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <SearchCity 
+            value={this.state.keyword}
+            onChange={this.changeKeywordHandler}
+            />
           {/* end of search section */}
   
         </div> 
