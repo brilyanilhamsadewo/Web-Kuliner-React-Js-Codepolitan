c4ed55d2826f58391f1b74c02f65003d113cc653
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2020-12-19 17:10:53 +0700

    Membuat Categories

--- src/pages/City.js
+++ src/pages/City.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
-import axios from 'axios'
-import { API } from '../config/api'
+import axios from 'axios';
+import { API } from '../config/api';
+
+const categoriesDummy = [
+    {
+      "categories": {
+        "id": 1,
+        "name": "Delivery"
+      }
+    },
+    {
+      "categories": {
+        "id": 2,
+        "name": "Dine-out"
+      }
+    },
+    {
+      "categories": {
+        "id": 3,
+        "name": "Nightlife"
+      }
+    },
+    {
+      "categories": {
+        "id": 4,
+        "name": "Catching-up"
+      }
+    }
+  ]
 
 class City extends Component {
     constructor() {
         super()
         this.state = {
-            city: null
+            city: null,
+            categories: null
         }
     }
+
+    transformCategoriesData(categories) {
+        let categoriesTransformed = categories.map(category => { 
+          return category.categories
+        })
+    
+        return categoriesTransformed
+    };
 
     getCityData = (city_id) => {
         let url = `${API.zomato.baseUrl}/cities`
@@ -31,6 +67,9 @@ class City extends Component {
         // cara mendapatkan parameter city_id dari url / route
         let { city_id } = this.props.match.params
         this.getCityData(city_id)
+
+        let categories = this.transformCategoriesData(categoriesDummy)
+        this.setState({ categories });
         }
     
 
@@ -46,6 +85,24 @@ class City extends Component {
                     </div>
                 </div>
             )}
+
+            <div className="row">
+                <div className="col-3">
+                <h5>Categories</h5>
+                {this.state.categories && (
+                    <div className="list-group">
+                    {this.state.categories.map(category => (
+                        <button
+                            key={category.id}
+                            className={'list-group-item list-group-item-action '}
+                        >
+                        {category.name}
+                        </button>
+                    ))}
+                    </div>
+                )}
+                </div>
+            </div>
         </div>
     )
   }
