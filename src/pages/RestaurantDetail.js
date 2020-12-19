b0841b96b946a56c1c81ddfc94092ea47c7caad5
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2020-12-20 00:16:44 +0700

    Mendapatkan Data Restaurant Reviews

--- src/pages/RestaurantDetail.js
+++ src/pages/RestaurantDetail.js
@@ -9,12 +9,30 @@ class RestaurantDetail extends Component {
         super()
         this.state = {
           restaurant: null,
+          reviews: null,
         }
+    }
+
+    getReviewsData = (restaurant_id) => {
+        let url = `${API.zomato.baseUrl}/reviews`
+        axios.get(url, {
+          headers: {
+            'user-key': API.zomato.api_key
+          },
+          params: {
+            res_id: restaurant_id
+          }
+        })
+          .then(({ data }) => {
+            this.setState({ reviews: data.user_reviews })
+          })
+          .catch(err => console.log(err))
     }
 
     componentDidMount () {
         let { params } = this.props.match
         this.getRestaurantData(params.restaurant_id)
+        this.getReviewsData(params.restaurant_id)
     }
 
     getRestaurantData = (restaurant_id) => {
